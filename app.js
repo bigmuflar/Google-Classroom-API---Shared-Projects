2f855cec224d2a3c7e5a4e98d9728f53d435d5c6
bigmuflar <jmdegonda@gmail.com>
2016-10-18 11:29:13 -0600

    update Google Classroom API Collaboration tool

--- app.js
+++ app.js
@@ -23,44 +23,44 @@ var oCtrl = this;
     oCtrl.greeting = "Origins";
 
     <!--Google Signin & button -->
-//      var googleUser = {};
-//      var startApp = function() {
-//        gapi.load('auth2', function(){
-//          // Retrieve the singleton for the GoogleAuth library and set up the client.
-//          auth2 = gapi.auth2.init({
-//            client_id: '553892757728-cev6cf803s6efjl7rgfsnp5tsrknuram.apps.googleusercontent.com',
-//            cookiepolicy: 'http://localhost:8080',
-//            // Request scopes in addition to 'profile' and 'email'
-//            //scope: 'additional_scope'
-//          });
-//          attachSignin(document.getElementById('signupBtn'));
-//        });
-//      };
-//
-//      function attachSignin(element) {
-//        console.log(element.id);
-//        auth2.attachClickHandler(element, {},
-//            function(googleUser) {
-//              document.getElementById('name').innerText = "Signed in: " +
-//                  googleUser.getBasicProfile().getName();
-//            }, function(error) {
-//              alert(JSON.stringify(error, undefined, 2));
-//            });
-//      
-//        startApp();
-//      };
-//    // auth2 is initialized with gapi.auth2.init() and a user is signed in.
-//    function signedin(){
-//        if (auth2.isSignedIn.get()) {
-//            var profile = auth2.currentUser.get().getBasicProfile();
-//            console.log('ID: ' + profile.getId());
-//            console.log('Full Name: ' + profile.getName());
-//            console.log('Given Name: ' + profile.getGivenName());
-//            console.log('Family Name: ' + profile.getFamilyName());
-//            console.log('Image URL: ' + profile.getImageUrl());
-//            console.log('Email: ' + profile.getEmail());
-//        }else{
-//            console.log('Not logged in');
-//        };
-//    };
+      var googleUser = {};
+      var startApp = function() {
+        gapi.load('auth2', function(){
+          // Retrieve the singleton for the GoogleAuth library and set up the client.
+          auth2 = gapi.auth2.init({
+            client_id: '553892757728-cev6cf803s6efjl7rgfsnp5tsrknuram.apps.googleusercontent.com',
+            cookiepolicy: 'http://localhost:8080',
+            // Request scopes in addition to 'profile' and 'email'
+            //scope: 'additional_scope'
+          });
+          attachSignin(document.getElementById('signupBtn'));
+        });
+      };
+
+      function attachSignin(element) {
+        console.log(element.id);
+        auth2.attachClickHandler(element, {},
+            function(googleUser) {
+              document.getElementById('name').innerText = "Signed in: " +
+                  googleUser.getBasicProfile().getName();
+            }, function(error) {
+              alert(JSON.stringify(error, undefined, 2));
+            });
+      
+        startApp();
+      };
+    // auth2 is initialized with gapi.auth2.init() and a user is signed in.
+    function signedin(){
+        if (auth2.isSignedIn.get()) {
+            var profile = auth2.currentUser.get().getBasicProfile();
+            console.log('ID: ' + profile.getId());
+            console.log('Full Name: ' + profile.getName());
+            console.log('Given Name: ' + profile.getGivenName());
+            console.log('Family Name: ' + profile.getFamilyName());
+            console.log('Image URL: ' + profile.getImageUrl());
+            console.log('Email: ' + profile.getEmail());
+        }else{
+            console.log('Not logged in');
+        };
+    };
 };
--- secondary.js
+++ secondary.js
@@ -1,10 +1,15 @@
 angular.module('lessonlace', ['ngRoute', 'infinite-scroll'])
     .controller("Signin", Signin)
-    .controller("Signup", Signup) .controller("infiniteScrollController", infiniteScrollController)
+    .controller("Signup", Signup)
+    
+   .controller("authenticate", authenticate) .controller("infiniteScrollController", infiniteScrollController)
     .controller("MainController", MainController)
     .controller("RedController", RedController)
     .controller("GreenController", GreenController)
     .controller("BlueController", BlueController)
+    .factory('userFact', userFactory);
+
+infiniteScrollController.$inject=["userFact"];
 
 angular.module('lessonlace')
     .config(myRouter);
@@ -35,6 +40,103 @@ angular.module('lessonlace')
             redirectTo: '/templates/signup.html'
         })
     }
+
+function authenticate(){
+    var aCtrl = this;
+    console.log('Testing Auth');
+      var CLIENT_ID = '553892757728-cev6cf803s6efjl7rgfsnp5tsrknuram.apps.googleusercontent.com';
+
+      var SCOPES = ["https://www.googleapis.com/auth/classroom.courses.readonly", "https://www.googleapis.com/auth/classroom.coursework.students","https://www.googleapis.com/auth/classroom.profile.photos","https://www.googleapis.com/auth/classroom.profile.emails"];
+
+      /**
+       * Check if current user has authorized this application.
+       */
+      aCtrl.handleAuthClick = function(event) {
+        console.log('here we are!');
+        gapi.auth.authorize(
+          {
+            'client_id': CLIENT_ID,
+            'scope': SCOPES.join(' '),
+            'immediate': true
+          }, aCtrl.handleAuthResult);
+      };
+
+      /**
+       * Handle response from authorization server.
+       *
+       * @param {Object} authResult Authorization result.
+       */
+      aCtrl.handleAuthResult = function(authResult) {
+        var authorizeDiv = document.getElementById('authorize-div');
+        if (authResult && !authResult.error) {
+          // Hide auth UI, then load client library.
+          authorizeDiv.style.display = 'none';
+          aCtrl.loadClassroomApi();
+        } else {
+          // Show auth UI, allowing the user to initiate authorization by
+          // clicking authorize button.
+          authorizeDiv.style.display = 'inline';
+        }
+      }
+
+      /**
+       * Initiate auth flow in response to user clicking authorize button.
+       *
+       * @param {Event} event Button click event.
+       */
+      aCtrl.handleAuthClick = function(event) {
+        gapi.auth.authorize(
+          {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
+          aCtrl.handleAuthResult);
+        return false;
+      }
+
+      /**
+       * Load Classroom API client library.
+       */
+      aCtrl.loadClassroomApi = function() {
+        gapi.client.load('classroom', 'v1', listCourses);
+      }
+
+      /**
+       * Print the names of the first 10 courses the user has access to. If
+       * no courses are found an appropriate message is printed.
+       */
+      function listCourses() {
+          console.log('It is hitting listCouses')
+        var request = gapi.client.classroom.courses.list({
+          pageSize: 10
+        });
+
+        request.execute(function(resp) {
+          var courses = resp.courses;
+          appendPre('Courses:');
+
+          if (courses.length > 0) {
+            for (i = 0; i < courses.length; i++) {
+              var course = courses[i];
+              appendPre(course.name)
+            }
+          } else {
+            appendPre('No courses found.');
+          }
+
+        });
+      }
+
+      /**
+       * Append a pre element to the body containing the given message
+       * as its text node.
+       *
+       * @param {string} message Text to be placed in pre element.
+       */
+      appendPre = function(message) {
+          console.log('It is hitting appendPre');
+        var pre = document.getElementById('output');
+        var textContent = document.createTextNode(message + '\n');
+        pre.appendChild(textContent);
+      }
+}
 
 function Signin() {
     var SigninCtrl = this;
@@ -71,432 +173,24 @@ function RedController(){
      bCtrl.greeting = 'This is the Blue Control';
  }
 
-function infiniteScrollController($scope, $http){
-    $scope.users =
-    [{
-            "id": 1,
-            "first_name": "Kimberly",
-            "last_name": "Grant",
-            "email": "kgrant0@ebay.co.uk",
-            "country": "Bolivia",
-            "ip_address": "93.77.148.179"
-        }, {
-            "id": 2,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 3,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 4,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 5,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 6,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 7,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 8,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 9,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 10,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 11,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 12,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 13,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 14,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 15,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 16,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 17,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 18,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 19,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 20,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 21,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 22,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 23,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 24,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 25,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 26,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 27,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 28,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 29,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 30,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 1,
-            "first_name": "Kimberly",
-            "last_name": "Grant",
-            "email": "kgrant0@ebay.co.uk",
-            "country": "Bolivia",
-            "ip_address": "93.77.148.179"
-        }, {
-            "id": 2,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 3,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 4,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 5,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 6,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 7,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 8,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 9,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 10,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 11,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 12,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 13,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 14,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 15,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 16,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 17,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 18,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 19,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 20,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 21,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 22,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 23,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 24,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 25,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 26,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 27,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 28,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 29,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }, {
-            "id": 30,
-            "first_name": "Elizabeth",
-            "last_name": "Lewis",
-            "email": "elewis1@so-net.ne.jp",
-            "country": "Indonesia",
-            "ip_address": "39.187.159.25"
-        }];
-     $scope.data =  $scope.users.slice(0, 30);
-     $scope.getMoreData = function () {
-     $scope.data =  $scope.users.slice(0,  $scope.data.length + 30);
-}
+function infiniteScrollController(userFact){
+    var scroll = this;
+    console.log('hello!');
+    
+    scroll.courses = userFact.courses;
+    
+//    scroll.data =  scroll.courses.slice(0, 30);
+//    scroll.getMoreData = function () {
+//    scroll.data =  scroll.courses.slice(0,  scroll.data.length + 30);
+//}
 };
 
+function userFactory($http){
+    console.log("hitting Factory")
+    $http.get('https://prod-day0-classroom.sandbox.googleapis.com/v1/courses/?key=AIzaSyCXHTtrF9QPuxU7IV22G8THnP7k9-AoUJU')
+    .then(function(res, status){
+        console.log("Success:", res);
+    }, function(res, status){
+        console.log("Failure:", status);
+    });
+}
